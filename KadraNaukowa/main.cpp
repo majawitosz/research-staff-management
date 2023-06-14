c4da39e4e5b3c64e8eb61ee48381cc7dd47495b6
majawitosz1 <majawitosz@gmail.com>
2023-06-14 16:31:14 +0200

    setup

---
+++ KadraNaukowa/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KadraNaukowa
+//
+//  Created by Maja Witosz on 14/06/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
