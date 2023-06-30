2532dbb08ba727f7f9cd0887bb3f57a92b0ab5e8
majawitosz1 <majawitosz@gmail.com>
2023-06-30 19:22:09 +0200

    .

---
+++ Build/Intermediates.noindex/KadraNaukowa.build/Release/KadraNaukowa.build/Objects-normal/arm64/Field.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/majawitosz/Documents/Studia/wszystkoProjekt/research-staff-management/KadraNaukowa/Field.cpp \
+  /Users/majawitosz/Documents/Studia/wszystkoProjekt/research-staff-management/KadraNaukowa/Field.hpp
---
+++ Build/Intermediates.noindex/KadraNaukowa.build/Release/KadraNaukowa.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/majawitosz/Documents/Studia/wszystkoProjekt/research-staff-management/KadraNaukowa/main.cpp \
+  /Users/majawitosz/Documents/Studia/wszystkoProjekt/research-staff-management/KadraNaukowa/Scientist.hpp \
+  /Users/majawitosz/Documents/Studia/wszystkoProjekt/research-staff-management/KadraNaukowa/Field.hpp \
+  /Users/majawitosz/Documents/Studia/wszystkoProjekt/research-staff-management/KadraNaukowa/View.hpp
